151323dc0c19d3b3483e791ce70111f3788966de
Guillaume Roussin <guillaume.roussin44@gmail.com>
2021-12-21 23:13:11 +0100

    coming soon teaser page

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style>
+
+html, body, #app {
+  margin: 0;
+  height: 100%;
+}
+
+@font-face {
+  font-family: 'Spartan';
+  src: url('./assets/fonts/league-spartan.otf');
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="center-main">
+    <img src="@/assets/img/logo.png" alt="" id="logo">
+    <h1 id="title">Coming soon</h1>
+  </div>
+</template>
+
+<style>
+
+#app {
+  display: grid;
+  place-items: center;
+
+  background: url("../assets/img/background.jpeg") center center no-repeat;
+  background-size: cover;
+}
+
+#center-main {
+  padding: 20px;
+}
+
+#logo {
+  width: 80%;
+  max-width: 500px;
+}
+
+#title {
+  font-family: 'Spartan', sans-serif;
+  color: #fff;
+
+  text-shadow: 0 0 5px #000;
+
+  text-transform: uppercase;
+  font-size: 3em;
+}
+
+</style>
+
+<script>
+
+export default {
+  name: 'Home'
+}
+</script>
